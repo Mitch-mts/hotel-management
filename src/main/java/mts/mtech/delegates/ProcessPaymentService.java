7c3a978f437e3641e0e2e6e9ae722d9e99c6135e
mitchell.severa <mitchell.severa@entelect.co.za>
2023-05-03 18:31:38 +0200

    initial commit

---
+++ src/main/java/mts/mtech/delegates/ProcessPaymentService.java
@@ -0,0 +1,11 @@
+package mts.mtech.delegates;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class ProcessPaymentService implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+
+    }
+}
