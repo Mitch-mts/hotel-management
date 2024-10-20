ccc17ada9ccb39363bb3b6a9a44ec01804ae5632
mitchell.severa <mitchell.severa@entelect.co.za>
2024-10-20 11:40:46 +0200

    updated code base

---
+++ src/main/resources/META-INF/resources/webjars/camunda/app/cockpit/scripts/config.js
@@ -0,0 +1,15 @@
+export default {
+    customScripts: [
+        // 'scripts/libraries.js',
+        // 'scripts/historyPluginRuntimeTab.js',
+        // 'scripts/historyPluginCockpitRoute.js',
+        // 'scripts/bpmn-viewer.development.js'
+    ],
+
+    bpmnJs: {
+        additionalModules: [
+        ],
+    },
+    disableWelcomeMessage: true,
+    previewHtml: true
+};
---
+++ src/main/resources/META-INF/resources/webjars/camunda/app/cockpit/scripts/libraries.js
@@ -0,0 +1,17 @@
+const jquery = document.createElement('script');
+jquery.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+jquery.type = 'text/javascript';
+jquery.integrity = 'sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=';
+jquery.crossOrigin = 'anonymous';
+
+const bootstrapJS = document.createElement('script');
+bootstrapJS.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js';
+bootstrapJS.type = 'text/javascript';
+bootstrapJS.integrity = 'sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p';
+bootstrapJS.crossOrigin = 'anonymous';
+
+const bootstrapCSS = document.createElement('script');
+bootstrapCSS.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+bootstrapCSS.type = 'text/stylesheet';
+bootstrapCSS.integrity = 'sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3';
+bootstrapCSS.crossOrigin = 'anonymous';
